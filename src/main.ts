2d97d68c8b7792ebcedaa98630f686f14a4b8752
EtErNaLiFe12 <wnsqja8888@gmail.com>
2022-02-26 18:16:49 +0900

    feat: swagger setting

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  BigInt.prototype['toJSON'] = function() { 
+    return this.toString(); 
+  };
+
+  const swaggerConfig = new DocumentBuilder()
+   .setTitle('bday-swagger')
+   .setDescription('Study-Project')
+   .setVersion('1.0')
+   .build();
+
+  const setDoc = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, setDoc);
+
   await app.listen(3000);
 }
 bootstrap();

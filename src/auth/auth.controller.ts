935fb1494996662d290eea85dab7e96ac5d7481b
EtErNaLiFe12 <wnsqja8888@gmail.com>
2022-02-26 18:17:33 +0900

    feat: jwt login

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,17 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { ApiOperation, ApiProperty, ApiPropertyOptional, ApiTags } from '@nestjs/swagger';
+import { AuthService } from './auth.service';
+import { AuthDto } from './dto/auth.dto';
+
+@Controller('auth')
+@ApiTags('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('login')
+  @ApiOperation({ summary: '로그인 API', description: '로그인 실행' })
+    async login(@Body() { email, password }: AuthDto ) { 
+      return this.authService.login(email, password);
+  }
+}
+
